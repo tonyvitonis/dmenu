518a3aaca7b7833cb9148d9bfe69a8a6a75a1eb9
Tony <tony.vitonis@mail.com>
2023-08-28 19:29:55 -0400

    Customize config.h

--- config.h
+++ config.h
@@ -5,14 +5,14 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"courier prime:size=20"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	               /*    fg         bg    */
+	[SchemeNorm] = { "#009900", "#000000" },
+	[SchemeSel]  = { "#000000", "#009900" },
+	[SchemeOut]  = { "#009900", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
